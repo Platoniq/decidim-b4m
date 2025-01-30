7c9dc9175ba13d6e1c434c66efc9e50c9e17a2ea
Harrison Hassig <harrisonhassig@gmail.com>
2025-01-30 17:12:36 +0100

    Overrides spec and removed unused locales

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -11,11 +11,125 @@ checksums = [
     files: {
       # views
       "/app/views/layouts/decidim/footer/_mini.html.erb" => "ccead2f5f20557ea4db1501de943f82b"
-    },
+    }
+  },
+  {
     package: "decidim-accountability",
     files: {
       # locales
-      "/config/locales/accountability.bs.yml" => "ccead2f5f20557ea4db1501de943f82b"
+      "/config/locales/sr-CS.yml" => "f9c74b2e988d50ebab03466c2f0b2c96"
+    }
+  },
+  {
+    package: "decidim-admin",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "fffe5350d8a3bbb597be20711f20fe10"
+    }
+  },
+  {
+    package: "decidim-assemblies",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "9458919e748b6cb29f5c814741f76c81"
+    }
+  },
+  {
+    package: "decidim-blogs",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "bc15738f3428f1f8d3d305de621c039c"
+    }
+  },
+  {
+    package: "decidim-budgets",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "67da89fa0a87ffcf1719f0f0858427e6"
+    }
+  },
+  {
+    package: "decidim-comments",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "8b1b3a8eb65c45c8980a631d90180e8f"
+    }
+  },
+  {
+    package: "decidim-core",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "469583bb3473d9a4f4b3d09e32395053"
+    }
+  },
+  {
+    package: "decidim-debates",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
+    }
+  },
+  {
+    package: "decidim-dev",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "757e7a8c5b4ce164e9cc58bcdd2d6124"
+    }
+  },
+  {
+    package: "decidim-forms",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
+    }
+  },
+  {
+    package: "decidim-meetings",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "6aba80d17069aa3b654e66a6e0ff6fad"
+    }
+  },
+  {
+    package: "decidim-pages",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
+    }
+  },
+  {
+    package: "decidim-proposals",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "2d62d75779e2825a11fe9fcbd9229427"
+    }
+  },
+  {
+    package: "decidim-sortitions",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
+    }
+  },
+  {
+    package: "decidim-surveys",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
+    }
+  },
+  {
+    package: "decidim-system",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "1455fe8c075efd5b68d20c415ddf7d01"
+    }
+  },
+  {
+    package: "decidim-templates",
+    files: {
+      # locales
+      "/config/locales/sr-CS.yml" => "4648c80b07421d9e00ababa1d353fd1c"
     }
   }
 ]
