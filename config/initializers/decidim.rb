e8586782437203cd28732b8a9d2ee097364bbfec
Harrison Hassig <harrisonhassig@gmail.com>
2026-02-14 18:59:23 +0100

    Locales

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 Decidim.configure do |config|
+  # Sets the list of available locales for the whole application.
+  #
+  # When an organization is created through the System area, system admins will
+  # be able to choose the available languages for that organization. That list
+  # of languages will be equal or a subset of the list in this file.
+  config.available_locales = ENV.fetch("DECIDIM_AVAILABLE_LOCALES", "en,it,bg,el,bs").split(",").map(&:strip).map(&:to_sym)
+  # Or block set it up manually and prevent ENV manipulation:
+  # config.available_locales = %w(en ca es)
+
+  # Sets the default locale for new organizations. When creating a new
+  # organization from the System area, system admins will be able to overwrite
+  # this value for that specific organization.
+  config.default_locale = ENV.fetch("DECIDIM_DEFAULT_LOCALE", "en").to_sym
+
   config.omniauth_providers = {
     decidim: {
       enabled: true,
